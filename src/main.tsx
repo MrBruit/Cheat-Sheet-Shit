339dc9dbb1c715faa84e722bf0862dce583b16e0
Florent Goujon <gouflo@gmail.com>
2026-01-19 19:08:32 +0100

    Redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { BrowserRouter } from 'react-router-dom'
 //import './index.css'
 import App from './App.tsx'
 
+// Gère la redirection depuis 404.html
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, '', redirect.replace(location.origin, ''));
+}
+
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename="/Cheat-Sheet-Shit">
+  <BrowserRouter basename="/Cheat-Sheet-Shit">
     <App />
-    </BrowserRouter>,
+  </BrowserRouter>,
 )
